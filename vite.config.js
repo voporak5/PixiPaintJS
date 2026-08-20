55e294dd28af92dc1ccd7c658061ed9e9b6e8d09
Cameron Cintron <cameron_cintron@yahoo.com>
2026-08-20 12:26:39 -0500

    - edit config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "./",
+	base: "/PixiPaintJS/",
   server: {
     port: 8080,
     open: true,
